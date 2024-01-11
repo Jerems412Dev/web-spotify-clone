d08f455f2fb4f2e8815b9e6650f8a103a5bb76c1
Jerems412Dev <jeremyobiangdev@gmail.com>
2024-01-11 14:26:53 +0100

    login and register html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { LoginComponent } from './public/login/login/login.component';
+import { RegisterComponent } from './public/register/register/register.component';
 
 export const routes: Routes = [
-    { path: '**', component: PageNotFoundComponent }
+    { path: '', component: LoginComponent },
+    { path: 'register', component: RegisterComponent }
 ];
