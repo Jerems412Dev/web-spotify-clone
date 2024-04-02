3bf14cca55b9a822fb3dcced4e18d4ce9e682ccf
jeremyobiangdev <jeremyobiangdev@gmail.com>
2024-04-02 04:27:25 +0100

    dont sleep, finish the playlist first

--- src/app/protected/playlist/list-playlist-song/list-playlist-song.component.ts
+++ src/app/protected/playlist/list-playlist-song/list-playlist-song.component.ts
@@ -1,14 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-list-playlist-song',
-  standalone: true,
-  templateUrl: './list-playlist-song.component.html',
-  styleUrls: ['./list-playlist-song.component.css']
+    selector: 'app-list-playlist-song',
+    standalone: true,
+    templateUrl: './list-playlist-song.component.html',
+    styleUrls: ['./list-playlist-song.component.css'],
+    imports: [CommonModule]
 })
 export class ListPlaylistSongComponent implements OnInit {
+  showPlayPlaylist = false;
 
   constructor() { }
+
+  hogglePlayPausePlaylist() {
+    this.showPlayPlaylist = !this.showPlayPlaylist;
+  }
 
   ngOnInit() {
   }
--- src/app/protected/playlist/playlist/playlist.component.ts
+++ src/app/protected/playlist/playlist/playlist.component.ts
@@ -1,22 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { PlaylistDetailsComponent } from '../playlist-details/playlist-details.component';
 import { CommonModule } from '@angular/common';
+import { ListPlaylistSongComponent } from "../list-playlist-song/list-playlist-song.component";
 
 @Component({
-  selector: 'app-playlist',
-  standalone: true,
-  templateUrl: './playlist.component.html',
-  styleUrls: ['./playlist.component.css'],
-  imports: [PlaylistDetailsComponent, CommonModule]
+    selector: 'app-playlist',
+    standalone: true,
+    templateUrl: './playlist.component.html',
+    styleUrls: ['./playlist.component.css'],
+    imports: [PlaylistDetailsComponent, CommonModule, ListPlaylistSongComponent]
 })
 export class PlaylistComponent implements OnInit {
-  showPlayPlaylist = false;
-
-  constructor() { }
-
-  hogglePlayPausePlaylist() {
-    this.showPlayPlaylist = !this.showPlayPlaylist;
-  }
 
   ngOnInit() {
   }
